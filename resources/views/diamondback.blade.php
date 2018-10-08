3f3f63a6d534a6736dc49fd92524874652443ca7
Victor Mann <victorcmann@gmail.com>
2018-10-08 20:38:22 -0300

    inserindo img da historia view diamondback

--- resources/views/diamondback.blade.php
+++ resources/views/diamondback.blade.php
@@ -8,6 +8,7 @@
 <section class="container">
     <div class="row">
         <div class="col-md-12">
+            <img src="{{ url('/') }}/imgs/history.jpg">
             <p>A DIAMONDBACK é uma das grandes marcas de bicicletas presentes no mundo! Com sede em Kent, Washington, os produtos Diamondback são vendidos em muitos países, incluindo os Estados Unidos, Austrália, Canadá, Bangladesh, Reino Unido e Brasil.</p>
             <p>A marca foi fundada na Califórnia, em 1977, como uma marca radical de BMX e não demorou muito para que se expandisse para o novo mundo da Mountain Bike. Desde então, ciclistas do mundo todo começaram a optar pela qualidade, a tecnologia e o custo benefício da Diamondback.
             <p>A marca desembarcou no Brasil na década de 90 e em 1993 foi criada a famosa equipe Diamondback Racing (DBR). A partir daí, centenas de ciclistas competem com sucesso sob o patrocínio da Diamondback, além de compor a lista de marcas de bicicletas mais conhecidas do mundo.
