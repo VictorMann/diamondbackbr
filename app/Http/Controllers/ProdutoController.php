b8a7bdebba1a642b01561de9c8ce86ed0655c1da
Victor Mann <victorcmann@gmail.com>
2018-10-15 11:47:28 -0300

    obtendo lista de categorias em ProdutoController

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -8,8 +8,12 @@ use Illuminate\Http\Request;
 
 class ProdutoController extends Controller
 {
-    public function index ()
+    public function index (Categoria $categoria)
     {
-        // 
+        $produtos = Produto::select('titulo', 'ano', 'image', 'slug')
+        ->where('categoria_id', $categoria->id)
+        ->get();
+
+        return view('lista-produtos-categoria')->with('produtos', $produtos);
     }
 }
