9aa195dad58dae6d5b3514ad52278d2214b1e33b
Victor Mann <victorcmann@gmail.com>
2018-10-15 10:23:09 -0300

    criando ProdutoController

---
+++ app/Http/Controllers/ProdutoController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Categoria;
+use App\Produto;
+use Illuminate\Http\Request;
+
+class ProdutoController extends Controller
+{
+    public function index ()
+    {
+        // 
+    }
+}
