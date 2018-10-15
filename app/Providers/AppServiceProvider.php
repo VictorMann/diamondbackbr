bdb71b90155ce7407e6fddb63eee1ab6a194c160
Victor Mann <victorcmann@gmail.com>
2018-10-15 11:46:30 -0300

    view composer compartilhando variavel categorias com partial lista-categorias

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Categoria;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +14,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        view()->composer('partials.lista-categorias', function ($view) {
+            $view->with('categorias', Categoria::get());
+        });
     }
 
     /**
