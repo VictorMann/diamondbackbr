499f41a514ac19e33374b0244f4b5625f68917c0
Victor Mann <victorcmann@gmail.com>
2018-10-16 23:19:13 -0300

    adicionando conteudo lista-produtos-categoria

--- resources/views/lista-produtos-categoria.blade.php
+++ resources/views/lista-produtos-categoria.blade.php
@@ -5,4 +5,31 @@
     {{ ucfirst($request->segment(1)) }}
 @endsection
 
+@section('styles')
+<link rel="stylesheet" href="{{ url('/') }}/css/category.css">
+@endsection
 
+@section('content')
+<section class="container">
+    <h1>{{ $request->segment(1) }}</h1>
+    <div class="ctn-grade-img">
+        @forelse ($produtos as $produto)
+        <div class="ctn-product">
+            <a href="{{ url('/') . '/' . $produto->slug }}">
+                <figure class="product-img">
+                    <img src="{{ url('/') }}/imgs/products/{{ $produto->image }}">
+                </figure>
+                <h3 class="product-name">
+                    {{ $produto->titulo }}
+                </h3>
+            </a>
+        </div>
+        @empty
+            No product
+        @endforelse
+    </div>
+    <div class="text-center">
+        {{ $produtos->links() }}
+    </div>
+</section>
+@endsection
