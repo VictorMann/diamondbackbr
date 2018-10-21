af22083ebedd5b78f58385070ca35c7ecb42f4d8
Victor Mann <victorcmann@gmail.com>
2018-10-20 23:06:15 -0300

    aplicando classes para o conteudo da page diamondback

--- resources/views/diamondback.blade.php
+++ resources/views/diamondback.blade.php
@@ -14,12 +14,14 @@
         <div class="col-md-12">
             <h1 class="header-page">Um pouco de história</h1>
             <img src="{{ url('/') }}/imgs/history.jpg">
-            <p>A DIAMONDBACK é uma das grandes marcas de bicicletas presentes no mundo! Com sede em Kent, Washington, os produtos Diamondback são vendidos em muitos países, incluindo os Estados Unidos, Austrália, Canadá, Bangladesh, Reino Unido e Brasil.</p>
-            <p>A marca foi fundada na Califórnia, em 1977, como uma marca radical de BMX e não demorou muito para que se expandisse para o novo mundo da Mountain Bike. Desde então, ciclistas do mundo todo começaram a optar pela qualidade, a tecnologia e o custo benefício da Diamondback.
-            <p>A marca desembarcou no Brasil na década de 90 e em 1993 foi criada a famosa equipe Diamondback Racing (DBR). A partir daí, centenas de ciclistas competem com sucesso sob o patrocínio da Diamondback, além de compor a lista de marcas de bicicletas mais conhecidas do mundo.
-            <p>Fabricados pela indústria de Kinesis na China - fabricante de quadros de alumínio e fibra de carbono especializada em bicicletas, garfos e componentes - os produtos são considerados como um protótipo high-end em desenvolvimento.</p>
-            <p>Ao longo dos anos, a Diamondback tem se esforçado, constantemente para aperfeiçoar seus produtos, sempre associando projetos inovadores (como o Dual Response, o WCF, a suspensão Knucklebox) ao design inovador para uso em Cross Country e Mountain Bike. Os quadros e bicicletas, passam por um período de desenvolvimento e testes, de forma a acrescentar as melhores inovações tecnológicas, reduzir o peso, a rigidez e ter a melhor performance para o ciclista amador e o profissional.</p>
-            <p>Cada bicicleta Diamondbakc possui conjuntos distintos de equipamento para usar em variedades específicas transformando as bicicletas em um verdadeiro objeto para lazer ou performance.</p>
+            <div class="page-content">
+                <p>A DIAMONDBACK é uma das grandes marcas de bicicletas presentes no mundo! Com sede em Kent, Washington, os produtos Diamondback são vendidos em muitos países, incluindo os Estados Unidos, Austrália, Canadá, Bangladesh, Reino Unido e Brasil.</p>
+                <p>A marca foi fundada na Califórnia, em 1977, como uma marca radical de BMX e não demorou muito para que se expandisse para o novo mundo da Mountain Bike. Desde então, ciclistas do mundo todo começaram a optar pela qualidade, a tecnologia e o custo benefício da Diamondback.
+                <p>A marca desembarcou no Brasil na década de 90 e em 1993 foi criada a famosa equipe Diamondback Racing (DBR). A partir daí, centenas de ciclistas competem com sucesso sob o patrocínio da Diamondback, além de compor a lista de marcas de bicicletas mais conhecidas do mundo.
+                <p>Fabricados pela indústria de Kinesis na China - fabricante de quadros de alumínio e fibra de carbono especializada em bicicletas, garfos e componentes - os produtos são considerados como um protótipo high-end em desenvolvimento.</p>
+                <p>Ao longo dos anos, a Diamondback tem se esforçado, constantemente para aperfeiçoar seus produtos, sempre associando projetos inovadores (como o Dual Response, o WCF, a suspensão Knucklebox) ao design inovador para uso em Cross Country e Mountain Bike. Os quadros e bicicletas, passam por um período de desenvolvimento e testes, de forma a acrescentar as melhores inovações tecnológicas, reduzir o peso, a rigidez e ter a melhor performance para o ciclista amador e o profissional.</p>
+                <p>Cada bicicleta Diamondbakc possui conjuntos distintos de equipamento para usar em variedades específicas transformando as bicicletas em um verdadeiro objeto para lazer ou performance.</p>
+            </div>
         </div>
     </div>
 </section>
