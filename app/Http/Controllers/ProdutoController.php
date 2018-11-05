970988bba2898f608662ea542524cbfffa105b28
Victor Mann <victorcmann@gmail.com>
2018-11-05 01:25:09 -0200

    obtendo imagens do produto e lista de relacionados

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Categoria;
 use App\Produto;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 
 class ProdutoController extends Controller
@@ -25,10 +26,17 @@ class ProdutoController extends Controller
     {
         $produto = Produto::where('slug', $slug)->first();
         $categoria = Categoria::findOrFail($produto->categoria_id);
+        $images = DB::table('images_produtos')->where('produto_id', $produto->id)->get();
+        $relacionados = Produto::where('categoria_id', $produto->categoria_id)
+        ->orderBy('dt_create', 'desc')
+        ->take(4)
+        ->get();
 
         return view('produto')->with([
             'produto' => $produto,
-            'categoria' => $categoria
+            'categoria' => $categoria,
+            'images' => $images,
+            'relacionados' => $relacionados
         ]);
     }
 }
