c381379f87e67644477e4066ba9ded5026f39668
Victor Mann <victorcmann@gmail.com>
2018-11-13 01:20:11 -0200

    definindo rotas para revendas e api para lista de revendas por estado

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,10 @@ Route::get('diamondback', 'DiamondbackController@index')->name('diamondbacks.ind
 Route::get('p/{slug}', 'ProdutoController@show')->name('produtos.show');
 Route::get('seja-um-revendedor', 'LojaController@revendedor')->name('lojas.revendedor');
 Route::get('encontre-uma-loja', 'LojaController@lojas')->name('lojas.encontre');
+Route::get('encontre-uma-loja/{estado}', 'LojaController@lojas')->name('lojas.estado');
 Route::get('garantia', 'GarantiaController@index')->name('garantias.index');
 Route::get('contato', 'ContatoController@index')->name('contatos.index');
 Route::get('{categoria}', 'ProdutoController@index')->name('produtos.index');
+
+// api
+Route::get('api/revendas-per-estado', 'LojaController@listRevPerEstJson');
