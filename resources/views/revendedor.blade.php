739824320b180d838e070009e2652cf6d5c7522e
Victor Mann <victorcmann@gmail.com>
2018-11-12 22:12:26 -0200

    alterando conteudo textual da pagina revendedor

--- resources/views/revendedor.blade.php
+++ resources/views/revendedor.blade.php
@@ -8,16 +8,14 @@
 <section class="container rev">
     <h1 class="header-page">Seja um revendedor Diamondback</h1>
     <div class="page-content">
-        <p>A DIAMONDBACK é uma das marcas de bicicletas mais conhecidas marcas do mundo! São produtos para o lazer, atividades esportivas, melhoria da mobilidade e manutenção.</p>
+        <p>A DIAMONDBACK é uma das marcas de bicicletas mais conhecidas do mundo! São produtos para o lazer, atividades esportivas, melhoria da mobilidade e manutenção.</p>
         <p>Nossos parceiros são parte essencial em nossos negócios. E por conta disso, trabalhamos, exclusivamente com o segmento B2B, vendendo nossos produtos somente para pessoas jurídicas, mediante aprovação de cadastro.</p>
-        <p>Se você possui um negócio, tem interesse em ter os produtos DIAMONDBACK em sua loja e usufruir de todas as vantagens de um Revendedor Autorizado, além de comercializar produtos importados, de ótima qualidade e exclusivos no Brasil, então preencha o formulário abaixo.</p>
-        <p>Pedimos a gentileza que preencha TODOS os campos requeridos dessa ficha para análise junto à nossa empresa.</p>
-        <p>ATENÇÃO: Após preencher o formulário, não se esqueça de anexar uma cópia do CONTRATO SOCIAL para validação do seu cadastro.</p>
-        <p>Para dúvidas ou informações, em contato com nosso Depto. de Vendas pelo telefone: (11) 2824-3333 ou pelo e-mail: contato@diamondback.com.br de 2ª a 6ª feira das 8h às 18h para obter mais informações.</p>
+        <p>Se você possui um negócio, tem interesse em ter os produtos DIAMONDBACK em sua loja e usufruir de todas as vantagens de um Revendedor Autorizado, além de comercializar produtos importados, de ótima qualidade e exclusivos no Brasil</p>
+        <p>Para dúvidas ou informações, entre em contato com nosso Depto. de Vendas pelo telefone: (11) 2824-3333 ou pelo e-mail: contato@diamondback.com.br de 2ª a 6ª feira das 8h às 18h para obter mais informações.</p>
         <p>Invista no seu negócio, venha fazer parte deste time.</p>
     </div>
     <div>
-        <button class="btn btn-sm btn-primary">Deseja fazer parte do nosso time?</button>
+        <a href="{{ route('contatos.index') }}" class="btn btn-sm btn-primary">Deseja fazer parte do nosso time?</a>
     </div>
 </section>
 @endsection
