5fdbf032a697776e740a16b81e03423f254485e8
Victor Mann <victorcmann@gmail.com>
2018-11-13 01:23:23 -0200

    definindo metodo lojas e listRevPerEstJson

--- app/Http/Controllers/LojaController.php
+++ app/Http/Controllers/LojaController.php
@@ -3,11 +3,33 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Revenda;
 
 class LojaController extends Controller
 {
     public function revendedor ()
     {
         return view('revendedor');
+    } 
+
+    public function lojas ($estado = null)
+    {
+        if ($estado)
+        {
+            $estado = DB::table('estado')->where('abbr', $estado)->first();
+            if (!$estado) return redirect()->route('lojas.encontre');
+
+            return view('lojas-estado')->with([
+                'estado' => $estado->nome
+            ]);
+        }
+
+        return view('lojas');
+    }
+
+    public function listRevPerEstJson ()
+    {
+        return Revenda::qtdRevendasPerEstado();
     }
 }
