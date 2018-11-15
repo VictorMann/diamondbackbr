1938d896b35988a1cd61c73597bb13e48b2e8684
Victor Mann <victorcmann@gmail.com>
2018-11-14 22:49:15 -0200

    definindo dados da pagina revendas por estado e style

--- resources/views/lojas-estado.blade.php
+++ resources/views/lojas-estado.blade.php
@@ -1,11 +1,35 @@
 @extends("templates.master")
 
+@section("styles")
+<link rel="stylesheet" href="{{ url('/') }}/css/lojas-estado.css">
+@endsection
+
 @section("content")
 <div class="container">
     <div class="row">
         <div class="col-md-12">
-            <h1 class="header-page">{{ $estado }}</h1>
+            <h1 class="header-page">Revendas em {{ $estado }}<a href="{{ route('lojas.encontre') }}" class="btn btn-primary pull-right">voltar ao mapa <i class="glyphicon glyphicon-triangle-right"></i></a></h1>
         </div>
+    </div>
+    <div class="row">
+        @foreach ($revendas as $r)
+        <div class="col-md-6">
+            <div class="cartao">
+                <h4 class="title">{{ $r->nome }}</h4>
+                <ul class="list-unstyled body">
+                    <li>End: {{ $r->endereco }}</li>
+                    <li>Cidade: {{ $r->cidade }}, {{ $r->cep }}</li>
+                    <li>Bairro: {{ $r->bairro }}</li>
+                    <li>Telefone: {{ $r->telefone }}</li>
+                    <li>Email: {{ $r->email }}</li>
+                    <li>Site: <a href="http://{{ $r->site }}" target="_blank">{{ $r->site }}</a></li>
+                </ul>
+            </div>
+        </div>
+        @endforeach
+    </div>
+    <div class="text-center">
+        {{ $revendas->links() }}
     </div>
 </div>
 @endsection
