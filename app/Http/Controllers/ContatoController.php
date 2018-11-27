faaa8ea1db43f942af1c3203f579bc4da7c1f627
Victor Mann <victorcmann@gmail.com>
2018-11-27 00:14:46 -0200

    criando classe de envio de email p Contato e configurando

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -3,11 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Mail\ContatoShipped;
+use Illuminate\Support\Facades\Mail;
 
 class ContatoController extends Controller
 {
     public function index ()
     {
         return view("contato");
+    }
+
+    public function send (Request $request)
+    {
+        Mail::to(config('mail.from.address'))
+        ->send(new ContatoShipped(
+            (object) $request->only([
+                'name',
+                'phone',
+                'email',
+                'comment'
+            ])
+        ));
+
+        return back()->with('send-ok', true);
     }
 }
---
+++ app/Mail/ContatoShipped.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class ContatoShipped extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $contato;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($contato = null)
+    {
+        $this->contato = $contato;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this
+        ->subject('Contato - ' . $this->contato->name)
+        ->view('emails.contato');
+    }
+}
