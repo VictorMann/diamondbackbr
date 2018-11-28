6d51a6934474995f7bfef540874f6b5206695c2b
Victor Mann <victorcmann@gmail.com>
2018-11-27 22:19:06 -0200

    alterando a vitrine com os itens mais recentes

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,7 +10,7 @@ class HomeController extends Controller
 {
     public function index ()
     {
-        $produtos = Produto::take(6)->get();
+        $produtos = Produto::orderBy('dt_create', 'DESC')->take(6)->get();
         return view("home")->with([
             'produtos' => $produtos
         ]);
