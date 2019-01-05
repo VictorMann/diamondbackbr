ce8e3edbdbff0cb97f17e15023a87654b0199adf
Victor Mann <victorcmann@gmail.com>
2019-01-05 19:21:56 -0200

    definindo BASE_URL e substituindo consulta get da jquery por api fetch

--- public/js/app.js
+++ public/js/app.js
@@ -1,22 +1,27 @@
+// Url Base
+const BASE_URL = 'http://localhost/diamondback-laravel/public/';
+
 // valida envio do formulário
 $('#search').on('submit', function(e) {
     let valueBusca = this.elements.s.value.trim();
     if (!valueBusca) e.preventDefault();
 });
 
+
 // newsletter
 $('#form-newsletter').submit(function(e) {
     
     e.preventDefault();
 
-    let dado = {email: this.elements.email.value};
+    let dado = $.param({email: this.elements.email.value});
 
     this.elements.email.value = '';
 
-    $.get('http://localhost/laravel/public/cadastro/newsletter', dado, (data, status) => {
-        if (status == 'success') {
-            console.log(data);
-            alert("Diamondback » newsletter\n====================================\n\nEmail Cadastrado com sucesso!");
-        }
-    });
+    fetch(`${BASE_URL}cadastro/newsletter?${dado}`)
+    .then(res => res.ok ? res.text() : Promise.reject(res.statusText))
+    .then(res => {
+        console.log(res);
+        alert("Diamondback » newsletter\n====================================\n\nEmail Cadastrado com sucesso!");
+    })
+    .catch(console.log);
 });
